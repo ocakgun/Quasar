02fa956210de5c4d1be7a376be5b7794d3bf0c99
Abdul Hameed Akram <abdulhameed.akram@lampromellon.com>
2020-12-09 15:13:28 +0500

    dec updated

--- firrtl_black_box_resource_files.f
+++ firrtl_black_box_resource_files.f
@@ -1,3 +1,3 @@
-/home/abdulhameed.akram/Documents/SweRV-Chislified/gated_latch.v
-/home/abdulhameed.akram/Documents/SweRV-Chislified/dmi_wrapper.sv
-/home/abdulhameed.akram/Documents/SweRV-Chislified/el2_mem.sv
+/home/abdulhameed.akram/Downloads/SweRV-Chislified/gated_latch.v
+/home/abdulhameed.akram/Downloads/SweRV-Chislified/dmi_wrapper.sv
+/home/abdulhameed.akram/Downloads/SweRV-Chislified/mem.sv
